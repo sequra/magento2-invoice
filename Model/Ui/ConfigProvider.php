c069d2bcdd4446cede5ebb76864ccf69409b5e50
Mikel Martin <mikel@zhenit.com>
2018-02-09 12:36:41 +0100

    [chg] PSR1/PSR2 code standard

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -12,24 +12,26 @@ use Magento\Checkout\Model\ConfigProviderInterface;
 /**
  * Class ConfigProvider
  */
-final class ConfigProvider implements ConfigProviderInterface {
-	const CODE = 'sequra_invoice';
+final class ConfigProvider implements ConfigProviderInterface
+{
+    const CODE = 'sequra_invoice';
 
-	/**
-	 * Retrieve assoc array of checkout configuration
-	 *
-	 * @return array
-	 */
-	public function getConfig() {
-		return [
-			'payment' => [
-				self::CODE => [
-					'useStoredCard' => [
-						'1' => __( 'Yes' ),
-						'0' => __( 'No' )
-					]
-				]
-			]
-		];
-	}
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [
+            'payment' => [
+                self::CODE => [
+                    'useStoredCard' => [
+                        '1' => __('Yes'),
+                        '0' => __('No')
+                    ]
+                ]
+            ]
+        ];
+    }
 }
